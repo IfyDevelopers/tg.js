656cd84833bbf9df89a49b6b60d606eeeea59441
Vall <57489626+mrvall@users.noreply.github.com>
2024-11-16 14:33:16 +1000

    1

--- config.js
+++ config.js
@@ -2,5 +2,5 @@ module.exports = {
     version: '0.0.12',           // Версия бота
     authors: ['@VaLl110'],  // Авторы бота
     ownersid: [1084019563],    // ID владельцев бота
-    maxMessagesPerUser: 60,
+    maxMessagesPerUser: 100,
   };
