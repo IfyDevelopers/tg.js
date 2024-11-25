7be21f03dd5f536afa9968dd574c46ea1e632cfa
Vall <57489626+mrvall@users.noreply.github.com>
2024-11-25 15:00:16 +1000

    исправил бяку

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
     version: '0.0.15',           // Версия бота
-    authors: ['@VaLl110'],  // Авторы бота
+    authors: ['@vallikoff'],  // Авторы бота
     ownersid: [1084019563],    // ID владельцев бота
     newschannel: "@ifydevnews", // канал с новостями
     maxMessagesPerUser: 30, // ограничение по сохранению истории
